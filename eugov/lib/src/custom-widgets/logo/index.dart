48fc36519468364af4be5594e198207051ef09f2
Joao <joao_ricardi@hotmail.com>
2019-06-15 22:52:47 -0300

    fazer Imagem

--- eugov/lib/src/custom-widgets/logo/index.dart
+++ eugov/lib/src/custom-widgets/logo/index.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class Logo extends StatelessWidget {
-  var _assetsImage = new AssetImage("assets/logo.png");
+  var _assetsImage = new AssetImage(".../assets/homen.png");
 
   @override
   Widget build(BuildContext context) {
     return Image(
       image: _assetsImage, 
       //height: 600,
-      //fit: BoxFit.cover,
+      fit: BoxFit.cover,
     );
   }
 }
--- eugov/lib/src/pages/dash/index.dart
+++ eugov/lib/src/pages/dash/index.dart
@@ -1,6 +1,11 @@
+import 'package:eugov/src/custom-widgets/card_view/index.dart';
+import 'package:eugov/src/custom-widgets/logo/index.dart';
 import 'package:flutter/material.dart';
+import 'package:eugov/src/values/colors.dart';
 
 class Dash extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -15,7 +20,29 @@ class Dash extends StatelessWidget {
              icon: Icon(Icons.menu),
            ),
         
-         )
+         ),
+         body: Row(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: <Widget>[
+             Padding(
+               padding:EdgeInsets.only(top:32.0,right: 16,left: 16,bottom: 16),
+               child: Column(
+                 children: <Widget>[
+                   CardView(
+                     height:400,
+                     width: MediaQuery.of(context).size.width - 60,
+                     child: Column(
+                       children: <Widget>[
+                         Logo()
+                       ],
+                     ),
+                    
+                   ),
+                 ],
+               ),
+             ),
+           ],
+         ),
        )
       ],
       
